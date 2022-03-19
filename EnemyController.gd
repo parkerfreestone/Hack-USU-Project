0f3839ac923291bcb305cddde77ee1d631ed8439
parkerfreestone <parkerfreestone@gmail.com>
2022-03-18 22:28:46 -0600

    worked on player script

--- EnemyController.gd
+++ EnemyController.gd
@@ -2,20 +2,16 @@ extends KinematicBody2D
 
 const UP_DIRECTION := Vector2.UP
 
-export var speed := 600.0 
-export var gravity := 4500.0
+var SPEED := 600.0
+var GRAVITY := 4500.0
+
 
 var velocity := Vector2.ZERO
 
 
 var current_room = 0
-onready var player_node = get_node("Player")
-
 
 func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
-
-	if (current_room - player_node.players_room) < 3:
-		print(player_node.get_position_in_parent())
+	velocity.y += GRAVITY * delta
 
 	velocity = move_and_slide(velocity, UP_DIRECTION)
---
+++ parkers-code/WorldController.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+# func _ready():
+# 	$MainEnemy.connect("room_changed", $Player, "update_player_room")
