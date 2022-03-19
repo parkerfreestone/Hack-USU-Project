331cfacabccfc734249a25399917c0406716f231
parkerfreestone <parkerfreestone@gmail.com>
2022-03-18 21:22:08 -0600

    added gitignore

--- EnemyController.gd
+++ EnemyController.gd
@@ -1,16 +1,26 @@
 extends KinematicBody2D
 
+const UP_DIRECTION := Vector2.UP
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var speed := 600.0 
+export var gravity := 4500.0
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var velocity := Vector2.ZERO
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# CONTROL FLOW
+var current_room = 0
+var players_room = 0
+
+
+func _process(delta: float):
+	playrs_room = get_node("PlayerCharacter").current_room
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+
+
+	if (current_room - players_room) < 3:
+		velocity.x.lerp(
+
+	velocity = move_and_slide(velocity, UP_DIRECTION)
