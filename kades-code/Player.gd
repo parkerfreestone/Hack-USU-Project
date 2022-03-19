bbd8920a86332e40f70e09f583e357c40837ddd0
Kade Angell <ponmlk523@gmail.com>
2022-03-19 16:02:41 -0600

    Final version of the game

--- kades-code/Player.gd
+++ kades-code/Player.gd
@@ -8,15 +8,19 @@ var ACCELERATION = 1
 
 var velocity = Vector2()
 var moving_left = false
+var key = false
 
 export var current_room = 0
+
+var stamina = 100
+var hidden = false
+var can_hide = false
+var alive = true
 
 func get_input():
 	if stamina < 100:
 		stamina += .5
 	velocity = Vector2()
-
-	print(stamina)
 
 	if moving_left:
 		$Sprite.flip_h = true
@@ -26,10 +30,10 @@ func get_input():
 	if Input.is_action_pressed("move_left"):
 		velocity.x += -ACCELERATION
 		moving_left = true
+		$AnimationPlayer.play("Walk")
 	if Input.is_action_pressed("move_right"):
 		velocity.x += ACCELERATION
 		moving_left = false
-<<<<<<< HEAD
 		$AnimationPlayer.play("Walk")
 	if Input.is_action_pressed("sprint") && stamina > 17:
 		SPEED = 60
@@ -41,17 +45,24 @@ func get_input():
 	if Input.is_action_just_released("sprint"):
 		SPEED = 15
 
-	if Input.is_action_pressed("hide"):
-		hidden = true
-		$AnimationPlayer.play("Hide")
+	if Input.is_action_pressed("hide") && !	velocity.x:
+		$AnimationPlayer.play("Crouch")
+		if can_hide:
+			hidden = true
+			get_parent().get_child(6).set_collision_layer_bit(0, false)
+			get_parent().get_child(6).set_collision_layer_bit(1, false)
+			$Sprite.set_modulate(Color(1, 1, 1, 0.5))
 	
+	if Input.is_action_just_released("hide"):
+		hidden = false	
+		get_parent().get_child(6).set_collision_layer_bit(0, true)
+		$Sprite.set_modulate(Color(1, 1, 1, 1))
+
 	if velocity.x == 0:
 		if !hidden:
 			$AnimationPlayer.play("Idle")
 		elif hidden:
-			$AnimationPlayer.play("Hide")
-=======
->>>>>>> 61262148a778c38e5106dc06042e9fee7a4ef1f3
+			$AnimationPlayer.play("Crouch")
 
 	velocity = velocity.normalized() * SPEED
 	
@@ -62,3 +73,9 @@ func _physics_process(delta):
 	get_input()
 	velocity.y += delta * GRAVITY
 	velocity = move_and_slide(velocity, UP_DIRECTION)
+
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		if collision.collider.name == "MainEnemy":
+			get_tree().change_scene("res://kades-code/Defeat.tscn")
+
